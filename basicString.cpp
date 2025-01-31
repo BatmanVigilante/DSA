d54c217b40da19b3eca06c1342c61ab5f3356c44
BatmanVigilante <ashishmohan625@gmail.com>
2025-01-31 22:00:24 +0530

    Strings

---
+++ basicString.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main(){
+    string s = "Visca Barca";
+    cout<<s[s.size()-1];
+
+    return 0;
+}
+// Compare this snippet from basicString.cpp:
