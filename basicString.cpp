eed550e58d03acb50860ae117854d50a10dad294
BatmanVigilante <ashishmohan625@gmail.com>
2025-02-03 20:30:15 +0530

    Longest Common Prefix

--- basicString.cpp
+++ basicString.cpp
@@ -2,9 +2,18 @@
 #include <iostream>
 using namespace std;
 int main(){
-    string s = "Visca Barca";
-    reverse(s.begin(),s.end());
-    cout<<s;
+    vector<string> x={"abc","bcad"};
+    //Longest common Prefix
+    sort(x.begin(),x.end());
+    string a=x[0];
+    string b=x[x.size()-1];
+    int minLen = min(a.size(),b.size());
+    int i=0;
+    for(i=0;i<minLen;i++){
+        if(a[i]!=b[i]) break;
+        if(i==0) cout<<"No common prefix";
+        else cout<<a.substr(0,i);
+    }
 
     return 0;
 }
