f52789becd55fcce4df2c2869c9b36e88978e789
BatmanVigilante <ashishmohan625@gmail.com>
2025-04-02 08:26:51 +0530

    Modified for printing purpose

--- Arrays/Medium/MaxSubArray.cpp
+++ Arrays/Medium/MaxSubArray.cpp
@@ -9,13 +9,23 @@ int main(){
         cin>>a[i];
     }
     int sum=0,ans=INT_MIN;
+    int start=-1,end = -1,ans_start=-1;
     for(int i=0;i<n;i++){
+        if(sum==0) start=i;
         sum+=a[i];
         if(sum<=0){
             sum=0;
         }
-        ans=max(sum,ans);
+        if(sum>ans){
+            ans=sum;
+            ans_start=start;
+            end=i;
+        }
     }
+    for(int i=ans_start;i<=end;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
     cout<<ans;
     return 0;
 }
