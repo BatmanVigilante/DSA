b3345be0c158d0aa7c2ff0844f9e81aa56151a99
BatmanVigilante <ashishmohan625@gmail.com>
2025-04-20 17:36:11 +0530

    Use Pointers

---
+++ BS/1dArrays/FindX.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int low = 0;
+    int high = n-1;
+    int mid = 0;
+    int key;
+    cin>>key;
+    while(low<=high){
+        mid = (low+high)/2;
+        if(arr[mid]==key){
+            cout<<mid<<endl;
+            return 0;
+        }
+        else if(arr[mid]<key){
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+
+return -1;
+}
